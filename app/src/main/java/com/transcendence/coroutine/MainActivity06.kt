6486bd645694619e06383d91a2f60eb6e118dd30
joephone <270136049@qq.com>
2025-06-27 10:39:32 +0800

    MainScope 生命周期是process级别的，即使Activity或Fragment已经被销毁，协程仍然在执行。

--- app/src/main/java/com/transcendence/coroutine/MainActivity06.kt
+++ app/src/main/java/com/transcendence/coroutine/MainActivity06.kt
@@ -3,39 +3,37 @@ package com.transcendence.coroutine
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.transcendence.coroutine.api.userServiceApi
 import com.transcendence.coroutine.log.LogUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 /**
  * @author joephone
- * @date 2025/6/27 9:14
+ * @date 2025/6/27 10:36
  * @description  MainScope  生命周期是process级别的，即使Activity或Fragment已经被销毁，协程仍然在执行。
  * @edition 1.0
  */
 class MainActivity06 : AppCompatActivity() {
 
-    var info :TextView? = null
+    private val mainScope = MainScope()
+    private var info :TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         info  = findViewById<TextView>(R.id.tv_info)
+        info?.text ="joe"
 
         val submit  = findViewById<TextView>(R.id.tv_submit).also {
             it.setOnClickListener {
-                // GlobalScope（全局作用域） 生命周期与 整个应用程序进程 绑定（协程会一直运行，直到进程结束或手动取消）
-                // launch是一个 协程启动函数，用于创建并启动一个新的协程
-                GlobalScope.launch(Dispatchers.Main) {  //父协程
-                    //挂起
-                    delay(10000)
-                    LogUtils.d("${Thread.currentThread().name}  delay")
+                mainScope.launch {
+                    val login = userServiceApi.loginSuspend("","123456")
+                    info?.text =" name:${login.data?.username}"
                 }
-                // 阻塞
-                Thread.sleep(10000)
-                LogUtils.d("${Thread.currentThread().name}  after sleep")
             }
         }
     }
